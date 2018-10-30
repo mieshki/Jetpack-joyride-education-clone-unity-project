ff64fa5ec3a23a06b0d12fb998aef3cb560206da
mieshki <miszek_12@wp.pl>
2018-10-30 22:34:55 +0100

    Set ground and sky border + added player

---
+++ Assets/Scripts/GamePhysics/PhysicsManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsManager : MonoBehaviour {
+
+    [Header("Physics settings")]
+    public Vector2 gravityVector = new Vector2(0f, -20f);
+
+	void Start () {
+        Physics2D.gravity = gravityVector;
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    [Header("MUST SET")]
+    public Rigidbody2D playerRB;
+
+
+    [Header("Player/physics settings")]
+    public float forceMultiplier = 10000f;
+
+
+    void MoveUp()
+    {
+        playerRB.AddForce(Vector2.up * Time.deltaTime * forceMultiplier);
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        
+        if (Input.GetKey("w")) MoveUp();
+
+	}
+}
