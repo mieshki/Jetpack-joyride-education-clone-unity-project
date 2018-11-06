77b5b18ed64dbab2208e78dfa443e1c56b7a7f63
Trixu <Trixxxu@gmail.com>
2018-11-06 19:14:33 +0100

    Adding destroyer to patterns

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    private void OnTransformChildrenChanged()
+    {
+        if(transform.childCount == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
